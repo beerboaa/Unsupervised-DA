4d62236ac77a6ea73db02e353e6d87ee066dff9d
Tuntai Boriboonthana <s1629339@ed.ac.uk>
2019-06-28 17:52:12 +0100

    add networks.py in method

--- method/networks.py
+++ method/networks.py
@@ -3,6 +3,8 @@ from torch import nn
 from torch.nn import init
 from torch.optim import lr_scheduler
 from torchvision import models
+import torch.nn.functional as F
+
 
 def init_weights(net, gain=0.02):
     def init_func(layer):
@@ -56,7 +58,115 @@ class Classifier(nn.Module):
     def forward(self, input):
         return self.fc(input)
 
-class LeNetEncoder
+class LeNetEncoder(nn.Module):
+    """LeNet encoder model for ADDA."""
+
+    def __init__(self):
+        """Init LeNet encoder."""
+        super(LeNetEncoder, self).__init__()
+
+        self.restored = False
+
+        self.encoder = nn.Sequential(
+            # 1st conv layer
+            # input [1 x 28 x 28]
+            # output [20 x 12 x 12]
+            nn.Conv2d(1, 20, kernel_size=5),
+            nn.MaxPool2d(kernel_size=2),
+            nn.ReLU(),
+            # 2nd conv layer
+            # input [20 x 12 x 12]
+            # output [50 x 4 x 4]
+            nn.Conv2d(20, 50, kernel_size=5),
+            nn.Dropout2d(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.ReLU()
+        )
+
+    def forward(self, input):
+        """Forward the LeNet."""
+
+        bs = input.size(0)
+        conv_out = self.encoder(input)
+        return conv_out.view(bs, -1)
+
+class LeNetClassifier(nn.Module):
+    """LeNet classifier model for ADDA."""
+
+    def __init__(self):
+        """Init LeNet encoder."""
+        super(LeNetClassifier, self).__init__()
+        self.fc1 = nn.Linear(50 * 5 * 5, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, feat):
+        """Forward the LeNet classifier."""
+        out = self.fc1(feat)
+        out = F.dropout(F.relu(feat), training=self.training)
+        out = self.fc2(out)
+        return out
+
+class DigitDiscriminator(nn.Module):
+    """
+    Discriminator network for digits
+    """
+    def __init__(self):
+        super(DigitDiscriminator, self).__init__()
+
+        self.discriminator = nn.Sequential(*[nn.Linear(512, 512),
+                                             nn.LeakyReLU(0.2),
+                                             # nn.Linear(512, 512),
+                                             # nn.LeakyReLU(0.2),
+                                             nn.Linear(512, 1)])
+
+    def forward(self, input):
+        return self.discriminator(input)
+
+class Discriminator(nn.Module):
+    """
+    Domain discriminator
+    """
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.discriminator = nn.Sequential(*[nn.Linear(2048, 1024),
+                                             nn.LeakyReLU(0.2),
+                                             nn.Linear(1024, 1024),
+                                             nn.LeakyReLU(0.2),
+                                             nn.Linear(1024, 1)])
+
+    def forward(self, input):
+        return self.discriminator(input)
+
+class GANLoss(nn.Module):
+    """
+    GANloss
+    """
+    def __init__(self, gan_mode, target_real_label=1.0, target_fake_label=0.0):
+        super(GANLoss, self).__init__()
+        self.register_buffer('real_label', torch.tensor(target_real_label))
+        self.register_buffer('fake_label', torch.tensor(target_fake_label))
+        self.gan_mode = gan_mode
+        if gan_mode == 'lsgan':
+            self.loss = nn.MSELoss()
+        elif gan_mode == 'vanilla':
+            self.loss = nn.BCEWithLogitsLoss()
+
+    def get_target_tensor(self, prediction, target_is_real):
+
+        if target_is_real:
+            target_tensor = self.real_label
+        else:
+            target_tensor = self.fake_label
+
+        return target_tensor.expand_as(prediction)
+
+    def __call__(self, prediction, target_is_real):
+
+        target_tensor = self.get_target_tensor(prediction, target_is_real)
+        loss = self.loss(prediction, target_tensor)
+        return loss
+
 
 
 
