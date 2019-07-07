621d54642308babc0b35f8dcbc697a0f4b54fe07
Tuntai Boriboonthana <s1629339@ed.ac.uk>
2019-07-08 00:54:17 +0100

    mlp

--- method/train.py
+++ method/train.py
@@ -126,11 +126,11 @@ def train(model, train_loader, test_loader, opt):
         # step the scheduler
         step_scheduler(schedulers)
 
-        if epoch < 5:
+        if epoch < 30:
             beta1 = 0.001
             beta2 = 0
 
-        elif epoch < 10:
+        elif epoch < 60:
             beta1 = 0.002
             beta2 = 0.002
 
